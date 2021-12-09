264ecd515fa853d828837742718c4e246af9afa6
fellipe <fellipejose2009@hotmail.com>
2021-12-08 22:03:08 -0300

    primeiro_commit

---
+++ rorapi/app/commands/authenticate_user.rb
@@ -0,0 +1,31 @@
+class AuthenticateUser
+    prepend SimpleCommand
+    
+    def initialize(email, password)
+        @email = email
+        @password = password
+    end
+    
+    def call      
+      JsonWebToken.encode(user_id: api_user.id) if api_user
+    end
+    
+    private
+    
+    attr_accessor :email, :password
+    
+    def api_user
+        user = User.find_by_email(email)
+        unless user.present?
+            errors.add :aviso, "Email ou senha Errada"
+            return nil
+        end
+                
+        unless user.valid_password?(password)
+            errors.add :aviso, "Email ou senha Errada"
+            return nil
+        end
+        
+        return user
+    end    
+end
