7c3c4544157453d722bf2a2521b4630d5983fbd0
VulcanoBr <vulcanorjbr@gmail.com>
2021-12-20 22:26:36 -0300

    Instalaçao Rpesc

---
+++ rorapi/spec/models/user_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+    describe 'New test' do
+        it 'Simple test' do
+            expect(true).to eq(true)
+        end
+    end
+end
